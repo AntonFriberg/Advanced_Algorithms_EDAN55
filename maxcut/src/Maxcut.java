dac0398f46b4d5b2da4e669f48f68933923b4baa
Anton Friberg <anton.friberg93@gmail.com>
2016-09-29 13:21:10 +0200

    Refactor maxcut

--- maxcut/src/Maxcut.java
+++ maxcut/src/Maxcut.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.nio.Buffer;
-import java.nio.file.Files;
 import java.util.Random;
 
 /**
@@ -11,16 +9,15 @@ public class Maxcut {
 
     private Random r = new Random();
     private int[][] edges;
-    private boolean[] choosenVertices;
+    private boolean[] chosenVertices;
     private int vertexSize;
-    private int numberOfRows;
 
     public static void main(String[] args) {
         String i1 = addDataPath("pw09_100.9.txt");
         String i2 = addDataPath("matching_1000.txt");
         Maxcut m = new Maxcut();
-        m.loadData(i1);
-        int iterations = 100000, maxSum = 0;
+        m.loadData(i1); //initialize graph from given data
+        int iterations = 100, maxSum = 0; // number of random sets to run
         for (int i = 0; i < iterations; i++) {
             int temp = m.algR();
             if (temp>maxSum) maxSum = temp;
@@ -29,23 +26,21 @@ public class Maxcut {
     }
 
     private int algR() {
-        /* coin flip to determine if vertex included in set or not */
-        choosenVertices = new boolean[vertexSize+1];
-        for (int i = 1; i <= vertexSize; i++) {
-            choosenVertices[i] = r.nextBoolean();
-        }
-
         int weightSum = 0;
+        /* coin flip to determine if vertex included in set or not */
+        chosenVertices = new boolean[vertexSize+1];
+        for (int i = 1; i <= vertexSize; i++) {
+            chosenVertices[i] = r.nextBoolean();
+        }
+        /* add weight to sum if both vertices chosen by coin flip */
         for (int i[]: edges) {
             int v1     = i[0];
             int v2     = i[1];
             int weight = i[2];
-
-            if (choosenVertices[v1] && choosenVertices[v2]) {
+            if (chosenVertices[v1] && chosenVertices[v2]) {
                 weightSum += weight;
             }
         }
-
         return weightSum;
     }
 
@@ -75,7 +70,6 @@ public class Maxcut {
                 int weight = Integer.parseInt(vertexProperties[2]);
                 edges[i++] = new int[]{v1,v2,weight};
             }
-            numberOfRows = i;
         } catch (IOException e) {
             e.printStackTrace();
         }
